f302be7c8aece06ec37c220951bc02a5fba432b8
victorahs <victorahsene90@gmail.com>
2018-03-13 14:51:35 +0100

    création StringUtilisTest.java

---
+++ src/test/java/demo_tests_unitaires/demo_tests_unitaires/StringUtilsTest.java
@@ -0,0 +1,34 @@
+package demo_tests_unitaires.demo_tests_unitaires;
+
+import org.junit.Test;
+
+import dev.utils.StringUtils;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+
+public class StringUtilsTest {
+	
+	@Test
+	public void testLevenshteinDistance(){
+		
+	int result = StringUtils.levenshteinDistance("chien", "chine");
+	assertEquals(2, result);
+	
+	int result1 = StringUtils.levenshteinDistance("chat", "chats");
+	assertEquals(1, result1);
+	
+	int result3 = StringUtils.levenshteinDistance("machins", "machines");
+	assertEquals(1, result3);
+	
+	int result4 = StringUtils.levenshteinDistance(null, null);
+	assertEquals(-1, result4);
+	
+	
+		
+		
+	}
+
+}
